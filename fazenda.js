c42e21ba7e79b365b9b8671a2ac704c06d2205ec
Robert Almeida <robertalmeida1597@gmail.com>
2020-10-08 09:03:56 -0300

    Salvando Respositório

---
+++ fazenda.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+
+let vez = 0
+async function x() {
+    for (let index = 0; index < 1500; index++) {
+
+        const browser = await puppeteer.launch({
+            headless: false
+        });
+        const page = await browser.newPage();
+        await page.goto('https://afazenda.r7.com/a-fazenda-12/votacao');
+        await page.click('#box_5f5fb3b7416eb9a8d0000891 > div > div > div > div > section > div.voting-view > figure:nth-child(1) > button');
+
+        await page.keyboard.press('Tab');
+        await page.keyboard.press('Enter');
+        await delay(2000);
+        await browser.close();
+        vez++
+        console.log('Votei ' + vez + ' ...');
+
+
+    }
+
+    x();
+}
+
+x();
+
+function delay(time) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+    });
+}
